5c99b6355a0b7f7c6e7b196422886c13742a8d3f
jingyun <531855995@qq.com>
2021-07-20 21:56:36 +0800

    first update commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 每一个组件都可以通过this直接访问$http
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
